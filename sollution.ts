e5ad8e47781cd7c62fb6bdbb381bb21a555ad1ef
Nazmul Hasan <mnhs1211@gmail.com>
2026-05-07 21:16:38 +0600

    Completed problem 7

--- sollution.ts
+++ sollution.ts
@@ -50,3 +50,54 @@ console.log(checkType("Hello"));
 
 console.log(checkType(42));
 
+
+
+// Problem 4:
+// Write a generic function getProperty that takes an object and a key, then returns the value of that key. Use constraints to ensure the key exists on the object.
+
+// Sample Input:
+// const user = { id: 1, name: "John Doe", age: 21 };
+// getProperty(user, "name");
+
+// Sample Output:
+// "John Doe";
+
+
+
+type typeProperty = { id: number, name: string, age: number }
+
+
+
+const getProperty : <T extends object, K extends keyof T>(obj: T, key: K) => T[K] = (obj, key) => {
+    return obj[key];
+}
+
+const user = { id: 1, name: "John Doe", age: 21 };
+
+getProperty(user, "name");
+
+
+
+
+// Problem 7:
+
+
+
+const getIntersection = (arr1 : number[], arr2: number[]) : number[] => {
+
+    const intersection : number[] = []
+
+    for(let i = 0; i<=arr1.length - 1; i++){
+        for(let j = 0; j <= arr2.length - 1; j++){
+            if(arr1[i] == arr2 [j]){
+                intersection.push(arr1[i]);
+            }
+        }
+    }
+
+    return intersection;
+
+}
+
+
+getIntersection([1, 2, 3, 4, 5], [3, 4, 5, 6, 7])
